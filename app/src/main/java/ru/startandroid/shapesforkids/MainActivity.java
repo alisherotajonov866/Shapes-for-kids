1f71db9644899325694d41e645a24ee76653e166
alisherotajonov <alisherotajonov@gmail.com>
2023-04-18 23:27:21 +0500

    added toolbar and changed manifest file

--- app/src/main/java/ru/startandroid/shapesforkids/MainActivity.java
+++ app/src/main/java/ru/startandroid/shapesforkids/MainActivity.java
@@ -2,13 +2,17 @@ package ru.startandroid.shapesforkids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
     }
 }
