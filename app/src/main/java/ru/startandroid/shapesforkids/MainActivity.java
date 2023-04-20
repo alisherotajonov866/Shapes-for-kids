500dd1b007b62a7fd378e49bd6ed45dbd35ea4b8
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-04-20 16:31:52 +0500

    Add shapes and PlayingGame some build.

--- app/src/main/java/ru/startandroid/shapesforkids/MainActivity.java
+++ app/src/main/java/ru/startandroid/shapesforkids/MainActivity.java
@@ -6,9 +6,12 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public boolean isBackPressed = false;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    //Status bar disappears when you tap the screen
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+
+
 }
--- app/src/main/java/ru/startandroid/shapesforkids/PlayingGame.java
+++ app/src/main/java/ru/startandroid/shapesforkids/PlayingGame.java
@@ -1,13 +1,21 @@
 package ru.startandroid.shapesforkids;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import ru.startandroid.shapesforkids.databinding.FragmentPlayingGameBinding;
 
@@ -24,14 +32,97 @@ public class PlayingGame extends Fragment {
     }
 
     private FragmentPlayingGameBinding binding;
+    ImageView shape_1;
+    ImageView shape_2;
+    ImageView shape_3;
+    TextView ansver;
+    LinearLayout shapeList;
+
+    ArrayList<ImageView> shapes = new ArrayList<ImageView>();
+    HashMap<String, Integer> shapesMap = new HashMap<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentPlayingGameBinding.inflate(inflater, container, false);
 
+        shape_1 = binding.shape1;
+        shape_2 = binding.shape2;
+        shape_3 = binding.shape3;
+        ansver = binding.ansver;
+
+        shapesMap.put("circle", R.drawable.circle);
+        shapesMap.put("polygon", R.drawable.polygon);
+        shapesMap.put("triangle", R.drawable.triangle);
+        shapesMap.put("rectangle", R.drawable.rectangle);
+        shapesMap.put("star", R.drawable.star);
+        shapesMap.put("pentagon", R.drawable.pentagon);
+
+        shapeList = binding.shapeList;
+        shapeList.setOnClickListener(v -> {
+            generateShapes();
+            return;
+        });
+
+        shapes.add(shape_1);
+        shapes.add(shape_2);
+        shapes.add(shape_3);
+        generateShapes();
+
         return binding.getRoot();
+
     }
+
+
+
+    private void generateShapes() {
+
+        for (ImageView shape : shapes) {
+            int random = (int) (Math.random()*6);
+            switch (random) {
+                case 0:
+                    shape.setImageResource(shapesMap.get("circle"));
+                    shape.setOnClickListener(v -> {
+                    ansver.setText("Aylana");
+                });
+                    break;
+
+                case 1:
+                    shape.setImageResource(shapesMap.get("polygon"));
+                    shape.setOnClickListener(v -> {
+                    ansver.setText("oltiburchak");
+                });
+                    break;
+                case 2:
+                    shape.setImageResource(shapesMap.get("triangle"));
+                    shape.setOnClickListener(v -> {
+                        ansver.setText("uchburchak");
+                    });
+                    break;
+                case 3:
+                    shape.setImageResource(shapesMap.get("rectangle"));
+                    shape.setOnClickListener(v -> {
+                        ansver.setText("to'rtburchak");
+                    });
+                    break;
+                case 4:
+                    shape.setImageResource(shapesMap.get("star"));
+                    shape.setOnClickListener(v -> {
+                        ansver.setText("yulduzcha");
+                    });
+                    break;
+                case 5:
+                    shape.setImageResource(shapesMap.get("pentagon"));
+                    shape.setOnClickListener(v -> {
+                        ansver.setText("beshburchak");
+                    });
+
+            }
+
+        }
+
+    }
+
 
     // we had better write these codes for onDestroyView
     @Override
